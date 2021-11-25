01fb53328e9994ee2515b12a66cb536a8c899478
ANKIT <ankitkumarraj596@gmail.com>
2021-11-25 20:30:27 +0530

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -10,11 +10,11 @@ app.use(express.json());
 const connectDB = require('./config/db');
 connectDB();
 
-//cors
-const corsOptions = {
-        origin: process.env.ALLOWED_CLIENTS.split(',')
-}
-app.use(cors(corsOptions));
+// //cors
+// const corsOptions = {
+//         origin: process.env.ALLOWED_CLIENTS.split(',')
+// }
+// app.use(cors(corsOptions));
 // template engine
 app.set('views',path.join(__dirname,'/views'));
 app.set('view engine','ejs');
